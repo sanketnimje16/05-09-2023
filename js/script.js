f1ce379430cdbaf62a0148ec44c7fc6a72fe72d7
sanketnimje16 <snimjesanket786@gmail.com>
2023-09-05 23:41:04 +0530

    concatenation

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Single Line comment 
+/*
+    multiine comment
+ */
+
+    var student1_name = "Sanket";
+    var student1_surname = "Nimje";
+
+    var student1 = { // 1. Property:value     || student1 =  Object
+        name : "Sanket",              // name = value
+        surname : "Nimje"
+
+    };
+    console.log("hello"+" "+student1.name); //Object.propertyname
+
+    console.log("Hello this is "+" "+ student1.name +" "+student1.surname);
+
+    
---
+++ js/script2.js
@@ -0,0 +1,7 @@
+const student4 = {//property:value
+    name:"Sanket",
+    father_name: "Prabhakarji",
+    surname: "Nimje"
+
+};
+document.write('<div>'+student4.name+'</div>');
